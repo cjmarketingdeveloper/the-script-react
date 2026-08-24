3cdd0130776b23306ff77d19a8454d9865e90931
Ndiwanga-sudo <dev2@cjmarketing.co>
2026-08-24 10:50:49 +0200

    updated changes

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -5,11 +5,9 @@ export const API_BASE         = "https://thescriptapiv1-632216c9063a.herokuapp.c
 export const SESSION_COOKIE   = "MEVENTWEBtimewaistengnewLXd5dkplaytheball";
 */
 
-
-export const API_URL          = "http://localhost:11566/api/";
-export const API_BASE         = "http://localhost:11566/";
-export const SESSION_COOKIE   = "theNO3ScriptSetionWork";
-
+export const API_URL          = process.env.REACT_APP_API_URL;
+export const API_BASE         = process.env.REACT_APP_API_BASE;
+export const SESSION_COOKIE   = process.env.REACT_APP_SESSION_COOKIE;
 
 export const VERSION          = "4.0.0";
 
